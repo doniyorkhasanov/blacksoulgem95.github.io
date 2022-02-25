ee2b7ec5d715ac19b53a50ce1a5cca374f37ba38
blacksoulgem95 <11537307+blacksoulgem95@users.noreply.github.com>
2022-02-25 09:08:59 +0100

    analytics

--- source/_components/ads/article.blade.php
+++ source/_components/ads/article.blade.php
@@ -1,11 +1,15 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6063500575269047"
-        crossorigin="anonymous"></script>
-<ins class="adsbygoogle"
-     style="display:block; text-align:center;"
-     data-ad-layout="in-article"
-     data-ad-format="fluid"
-     data-ad-client="ca-pub-6063500575269047"
-     data-ad-slot="1377606791"></ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+@if (isset($page->baseUrl))
+
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6063500575269047"
+            crossorigin="anonymous"></script>
+    <ins class="adsbygoogle"
+         style="display:block; text-align:center;"
+         data-ad-layout="in-article"
+         data-ad-format="fluid"
+         data-ad-client="ca-pub-6063500575269047"
+         data-ad-slot="1377606791"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+
+@endif
--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -1,92 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <meta name="description" content="{{ $page->description ?? $page->siteDescription }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="{{ $page->description ?? $page->siteDescription }}">
 
-        <meta property="og:title" content="{{ $page->title ? $page->title . ' | ' : '' }}{{ $page->siteName }}"/>
-        <meta property="og:type" content="{{ $page->type ?? 'website' }}" />
-        <meta property="og:url" content="{{ $page->getUrl() }}"/>
-        <link rel="canonical" href="{{ $page->getUrl() }}"/>
-        <meta property="og:description" content="{{ $page->description ?? $page->siteDescription }}" />
+    <meta property="og:title" content="{{ $page->title ? $page->title . ' | ' : '' }}{{ $page->siteName }}"/>
+    <meta property="og:type" content="{{ $page->type ?? 'website' }}"/>
+    <meta property="og:url" content="{{ $page->getUrl() }}"/>
+    <link rel="canonical" href="{{ $page->getUrl() }}"/>
+    <meta property="og:description" content="{{ $page->description ?? $page->siteDescription }}"/>
 
-        @stack('meta')
+    @stack('meta')
 
-        <title>{{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}</title>
+    <title>{{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}</title>
 
-        <link rel="home" href="{{ $page->baseUrl }}">
-        <link rel="icon" href="/favicon.ico">
-        <link href="/blog/feed.atom" type="application/atom+xml" rel="alternate" title="{{ $page->siteName }} Atom Feed">
+    <link rel="home" href="{{ $page->baseUrl }}">
+    <link rel="icon" href="/favicon.ico">
+    <link href="/blog/feed.atom" type="application/atom+xml" rel="alternate" title="{{ $page->siteName }} Atom Feed">
 
-        @if ($page->production)
-            <!-- Insert analytics code here -->
-        @endif
+    @if ($page->production)
+    <!-- Insert analytics code here -->
+    @endif
 
 
-        <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
-        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i"
+          rel="stylesheet">
+    <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
 
-        @include('_components.cookie-banner')
-    </head>
+    @include('_components.cookie-banner')
+</head>
 
-    <body class="flex flex-col justify-between min-h-screen bg-gray-100 text-gray-800 leading-normal font-sans">
-        <header class="flex items-center shadow bg-white border-b h-24 py-4" role="banner">
-            <div class="container flex items-center max-w-8xl mx-auto px-4 lg:px-8">
-                <div class="flex items-center">
-                    <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center">
-                        <img class="h-8 md:h-10 mr-3" src="/assets/img/logo.png" alt="{{ $page->siteName }} logo" />
+<body class="flex flex-col justify-between min-h-screen bg-gray-100 text-gray-800 leading-normal font-sans">
+<header class="flex items-center shadow bg-white border-b h-24 py-4" role="banner">
+    <div class="container flex items-center max-w-8xl mx-auto px-4 lg:px-8">
+        <div class="flex items-center">
+            <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center">
+                <img class="h-8 md:h-10 mr-3" src="/assets/img/logo.png" alt="{{ $page->siteName }} logo"/>
 
-{{--                        <h1 class="text-lg md:text-2xl text-pink-800 font-semibold hover:text-pink-600 my-0">{{ $page->siteName }}</h1>--}}
-                    </a>
-                </div>
+                {{--                        <h1 class="text-lg md:text-2xl text-pink-800 font-semibold hover:text-pink-600 my-0">{{ $page->siteName }}</h1>--}}
+            </a>
+        </div>
 
-                <div id="vue-search" class="flex flex-1 justify-end items-center">
-                    <search></search>
+        <div id="vue-search" class="flex flex-1 justify-end items-center">
+            <search></search>
 
-                    @include('_nav.menu')
+            @include('_nav.menu')
 
-                    @include('_nav.menu-toggle')
-                </div>
-            </div>
-        </header>
+            @include('_nav.menu-toggle')
+        </div>
+    </div>
+</header>
 
-        @include('_nav.menu-responsive')
+@include('_nav.menu-responsive')
 
-        @yield('head')
+@yield('head')
 
-        <main role="main" class="flex-auto w-full container max-w-4xl mx-auto py-16 px-6">
-            @yield('body')
-        </main>
+<main role="main" class="flex-auto w-full container max-w-4xl mx-auto py-16 px-6">
+    @yield('body')
+</main>
 
-        <footer class="bg-white text-center text-sm mt-12 py-4" role="contentinfo">
-            <ul class="flex flex-col md:flex-row justify-around list-none">
-                <li>
-                    &copy; {{ date('Y') }} - Sofia Vicedomini.
-                </li>
+<footer class="bg-white text-center text-sm mt-12 py-4" role="contentinfo">
+    <ul class="flex flex-col md:flex-row justify-around list-none">
+        <li>
+            &copy; {{ date('Y') }} - Sofia Vicedomini.
+        </li>
 
-                <li>
-                    @include("_components.random-quote")
-                </li>
+        <li>
+            @include("_components.random-quote")
+        </li>
 
-                <li>
-                    @foreach($socials as $social)
+        <li>
+            @foreach($socials as $social)
 
-                        <a href="{{$social->url}}" target="_blank" class=" @if(!$loop->last) mr-3 @endif text-2xl md:text-base ">
-                            <i class="{{$social->icon}}" aria-label="{{$social->title}}"></i>
-                        </a>
-                    @endforeach
-                </li>
+                <a href="{{$social->url}}" target="_blank"
+                   class=" @if(!$loop->last) mr-3 @endif text-2xl md:text-base ">
+                    <i class="{{$social->icon}}" aria-label="{{$social->title}}"></i>
+                </a>
+            @endforeach
+        </li>
 
-                <li>
-                    @include("_components.privacy-policy-button")
-                </li>
-            </ul>
-        </footer>
+        <li>
+            @include("_components.privacy-policy-button")
+        </li>
+    </ul>
+</footer>
 
-        <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
-        @stack('scripts')
-        @include("_components.adsense")
-    </body>
+<script src="{{ mix('js/main.js', 'assets/build') }}"></script>
+@stack('scripts')
+@if (isset($page->baseUrl))
+    @include("_components.adsense")
+    @include("_components.analytics.gtag")
+@endif
+</body>
 </html>
