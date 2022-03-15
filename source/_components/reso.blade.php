4eb446163fd41aa274f6d42b4629b5b2500070c8
blacksoulgem95 <11537307+blacksoulgem95@users.noreply.github.com>
2022-03-15 18:28:29 +0100

    terms

--- source/_components/reso.blade.php
+++ source/_components/reso.blade.php
@@ -6,4 +6,5 @@ The products must be returned in their original packaging.</p>
 
 <p>Any return to Sofia Vicedomini must first be authorized by us in writing and will be subject to administrative costs.</p>
 
+<p>For more information, please refer to the <a href="/shop/terms-and-conditions">Terms and Conditions</a>
 </div>
